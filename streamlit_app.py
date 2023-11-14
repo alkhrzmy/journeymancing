62949e030443f3a719f5447868d5fc1d462a9498
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2023-11-14 17:43:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ import streamlit as st
 import pandas as pd
 
 # Imports for AgGrid
-from st_aggrid import AgGrid, GridUpdateMode, JsCode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
+#from st_aggrid import AgGrid, GridUpdateMode, JsCode
+#from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 # --------------------------------------------
 
