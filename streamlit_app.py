ce6b8136be13a5f6dab406c7053751c9e9f1ce2f
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2023-11-14 17:39:39 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+# ----------------------Importing libraries----------------------
+
+import streamlit as st
+from streamlit_pills import pills
+import pandas as pd
+
+# Imports for AgGrid
+from st_aggrid import AgGrid, GridUpdateMode, JsCode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+
+# --------------------------------------------
+
+st.header('Kelompok 8 Tugas Besar Algoritma Pemrograman')
