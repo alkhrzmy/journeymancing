37d63c8be488f47211160dd03ba7e7a278346d51
Natasya Ega Lina Marbun <149855891+nateee15@users.noreply.github.com>
2023-11-14 18:21:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,40 @@
-# ----------------------Importing libraries----------------------
-
 import streamlit as st
-#from streamlit_pills import pills
 import pandas as pd
 
-# Imports for AgGrid
-#from st_aggrid import AgGrid, GridUpdateMode, JsCode
-#from st_aggrid.grid_options_builder import GridOptionsBuilder
+# Membuat DataFrame untuk menyimpan catatan memancing
+catatan_memancing = pd.DataFrame(columns=['Tanggal', 'Lokasi', 'Jenis Ikan', 'Cuaca', 'Peralatan', 'Catatan'])
 
-# --------------------------------------------
+# Fungsi untuk menambahkan catatan memancing
+def tambah_catatan(tanggal, lokasi, jenis_ikan, cuaca, peralatan, catatan):
+    global catatan_memancing
+    catatan_memancing = catatan_memancing.append({
+        'Tanggal': tanggal,
+        'Lokasi': lokasi,
+        'Jenis Ikan': jenis_ikan,
+        'Cuaca': cuaca,
+        'Peralatan': peralatan,
+        'Catatan': catatan
+    }, ignore_index=True)
 
-st.header('Kelompok 8 Tugas Besar Algoritma Pemrograman')
-st.text("1. Natasya Ega Lina Marbun   122450024")
+# Halaman utama
+def main():
+    st.title("Catatan Memancing")
+
+    # Sidebar untuk menambahkan catatan
+    st.sidebar.header("Tambah Catatan")
+    tanggal = st.sidebar.date_input("Tanggal", pd.to_datetime('today'))
+    lokasi = st.sidebar.text_input("Lokasi")
+    jenis_ikan = st.sidebar.text_input("Jenis Ikan")
+    cuaca = st.sidebar.text_input("Cuaca")
+    peralatan = st.sidebar.text_input("Peralatan")
+    catatan = st.sidebar.text_area("Catatan")
+    
+    if st.sidebar.button("Simpan Catatan"):
+        tambah_catatan(tanggal, lokasi, jenis_ikan, cuaca, peralatan, catatan)
+
+    # Menampilkan catatan yang telah disimpan
+    st.subheader("Catatan Memancing yang Telah Disimpan")
+    st.dataframe(catatan_memancing)
+
+if __name__ == "__main__":
+    main()
