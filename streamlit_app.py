6cac8aed0ebead12c7ee37ef435e419bf1ffdb30
Natasya Ega Lina Marbun <149855891+nateee15@users.noreply.github.com>
2023-11-17 16:27:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,27 @@ import pandas as pd
 from datetime import datetime
 import folium
 
+# Fungsi login
+def login():
+    st.title("Login Page")
+
+    # Data pengguna (dapat diganti dengan database)
+    user_data = {
+        "username": "admin",
+        "password": "admin123"
+    }
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if username == user_data['username'] and password == user_data['password']:
+            st.success("Login Berhasil!")
+            return True
+        else:
+            st.error("Username atau Password salah. Coba lagi.")
+    return False
+    
 # Fungsi untuk membuat tabel catatan memancing jika belum ada
 def create_table():
     conn = sqlite3.connect('fishing_notes.db')
@@ -62,45 +83,51 @@ def perform_analysis():
 # Membuat tabel jika belum ada
 create_table()
 
-# Judul halaman
-st.title('Catatan Memancing')
-
-# Formulir untuk menambahkan catatan memancing
-st.header('Tambah Catatan Memancing')
-tanggal = st.date_input('Tanggal')
-lokasi = st.text_input('Lokasi')
-jenis_alat = st.text_input('Jenis Alat')
-cuaca = st.text_input('Cuaca')
-jumlah_ikan = st.number_input('Jumlah Ikan', min_value=0, step=1)
-jenis_ikan = st.text_input('Jenis Ikan')
-ukuran_ikan = st.number_input('Ukuran Ikan (cm)', min_value=0.0)
-berat_ikan = st.number_input('Berat Ikan (kg)', min_value=0.0)
-latitude = st.number_input('Latitude')
-longitude = st.number_input('Longitude')
-if st.button('Tambah Catatan'):
-    add_fishing_note(tanggal, lokasi, jenis_alat, cuaca, jumlah_ikan, jenis_ikan, ukuran_ikan, berat_ikan, latitude, longitude)
-    st.success('Catatan berhasil ditambahkan!')
-
-# Menampilkan catatan memancing
-st.header('Catatan Memancing')
-catatan_memancing = show_fishing_notes()
-if catatan_memancing:
-    for catatan in catatan_memancing:
-        st.write(f"**Tanggal:** {catatan[1]}, **Lokasi:** {catatan[2]}, **Jenis Alat:** {catatan[3]}, **Cuaca:** {catatan[4]}, **Jumlah Ikan:** {catatan[5]}, **Jenis Ikan:** {catatan[6]}, **Ukuran Ikan:** {catatan[7]} cm, **Berat Ikan:** {catatan[8]} kg")
+#Login page
+if login():
+    # Judul halaman jika login berhasil
+    st.title('Catatan Memancing')
+    # Judul halaman
+    st.title('Catatan Memancing')
+    
+    # Formulir untuk menambahkan catatan memancing
+    st.header('Tambah Catatan Memancing')
+    tanggal = st.date_input('Tanggal')
+    lokasi = st.text_input('Lokasi')
+    jenis_alat = st.text_input('Jenis Alat')
+    cuaca = st.text_input('Cuaca')
+    jumlah_ikan = st.number_input('Jumlah Ikan', min_value=0, step=1)
+    jenis_ikan = st.text_input('Jenis Ikan')
+    ukuran_ikan = st.number_input('Ukuran Ikan (cm)', min_value=0.0)
+    berat_ikan = st.number_input('Berat Ikan (kg)', min_value=0.0)
+    latitude = st.number_input('Latitude')
+    longitude = st.number_input('Longitude')
+    if st.button('Tambah Catatan'):
+        add_fishing_note(tanggal, lokasi, jenis_alat, cuaca, jumlah_ikan, jenis_ikan, ukuran_ikan, berat_ikan, latitude, longitude)
+        st.success('Catatan berhasil ditambahkan!')
+    
+    # Menampilkan catatan memancing
+    st.header('Catatan Memancing')
+    catatan_memancing = show_fishing_notes()
+    if catatan_memancing:
+        for catatan in catatan_memancing:
+            st.write(f"**Tanggal:** {catatan[1]}, **Lokasi:** {catatan[2]}, **Jenis Alat:** {catatan[3]}, **Cuaca:** {catatan[4]}, **Jumlah Ikan:** {catatan[5]}, **Jenis Ikan:** {catatan[6]}, **Ukuran Ikan:** {catatan[7]} cm, **Berat Ikan:** {catatan[8]} kg")
+    else:
+        st.warning('Belum ada catatan memancing.')
+    
+    # Menampilkan peta dengan lokasi catatan memancing
+    st.header('Peta Lokasi Memancing')
+    if catatan_memancing:
+        m = folium.Map(location=[catatan_memancing[0][-2], catatan_memancing[0][-1]], zoom_start=10)
+        for catatan in catatan_memancing:
+            folium.Marker([catatan[-2], catatan[-1]], popup=f"Lokasi: {catatan[2]}", tooltip="Klik untuk info lebih lanjut").add_to(m)
+        st.folium_chart(m)
+    
+    # Analisis sederhana
+    st.header('Analisis Sederhana')
+    total_ikan, rata_rata_ukuran, rata_rata_berat = perform_analysis()
+    st.write(f"Total Ikan yang ditangkap: {total_ikan}")
+    st.write(f"Rata-rata Ukuran Ikan: {rata_rata_ukuran:.2f} cm")
+    st.write(f"Rata-rata Berat Ikan: {rata_rata_berat:.2f} kg")
 else:
-    st.warning('Belum ada catatan memancing.')
-
-# Menampilkan peta dengan lokasi catatan memancing
-st.header('Peta Lokasi Memancing')
-if catatan_memancing:
-    m = folium.Map(location=[catatan_memancing[0][-2], catatan_memancing[0][-1]], zoom_start=10)
-    for catatan in catatan_memancing:
-        folium.Marker([catatan[-2], catatan[-1]], popup=f"Lokasi: {catatan[2]}", tooltip="Klik untuk info lebih lanjut").add_to(m)
-    st.folium_chart(m)
-
-# Analisis sederhana
-st.header('Analisis Sederhana')
-total_ikan, rata_rata_ukuran, rata_rata_berat = perform_analysis()
-st.write(f"Total Ikan yang ditangkap: {total_ikan}")
-st.write(f"Rata-rata Ukuran Ikan: {rata_rata_ukuran:.2f} cm")
-st.write(f"Rata-rata Berat Ikan: {rata_rata_berat:.2f} kg")
+    st.stop()
