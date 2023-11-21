979e8b94aeb55dfc78d7150182fafa1c46d86bd6
Feryadi Yulius <133149348+strng-fer@users.noreply.github.com>
2023-11-21 23:16:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -194,14 +194,14 @@ if authentication_status:
     def main_page():
         st.title("Halaman Utama")
         
-    button_col1, button_col2, button_col3 = st.columns(3)
-    
-    if button_col1.button("Tambah Catatan"):
-        add_note()
-    elif button_col2.button("Edit Catatan"):
-        edit_note()
-    elif button_col3.button("Hapus Catatan"):
-        delete_note()
+        button_col1, button_col2, button_col3 = st.columns(3)
+        
+        if button_col1.button("Tambah Catatan"):
+            add_note()
+        elif button_col2.button("Edit Catatan"):
+            edit_note()
+        elif button_col3.button("Hapus Catatan"):
+            delete_note()
 
 
     def main():
