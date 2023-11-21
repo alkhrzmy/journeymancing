ba91ec2dda2d06906f00f8995fc50cd4b62ac65b
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2023-11-21 16:40:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,165 @@
 import streamlit as st
-import pandas as pd
-from sqlalchemy import create_engine
+import requests
+import datetime
+import requests
+import datetime
 
-# Fungsi untuk menghubungkan ke database SQL
-def create_connection():
-    username = 'root'
-    password = 'FarestaHaerz135'
-    host = 'localhost'
-    database_name = 'journeymancing123'
-    
-    # Buat string koneksi
-    connection_string = f"mysql+mysqlconnector://{username}:{password}@{host}/{database_name}"
-    
-    # Buat koneksi
-    engine = create_engine(connection_string)
+import pickle
+import streamlit_authenticator as stauth
+from pathlib import Path
 
-    return conn
+st.set_page_config(page_title="Journey Mancing", page_icon="🎣", layout="wide")
 
-# Fungsi untuk melakukan login
-def login(username, password):
-    conn = create_connection()
-    query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
-    result = conn.execute(query)
-    return result.fetchone()
+# ----- USER AUTHENTICATION
+names = ["admin", "Feryadi Yulius", "Gymnastiar Al Khoarizmy", "Natasya Ega Lina Marbun", "Khusnun Nisa"]
+usernames = ["admin", "feryadi", "jimnas", "natee", "khusnun"]
 
-# Fungsi untuk tampilan halaman login
-def login_page():
-    st.title("Journey Mancing")
-    st.markdown("## Silakan login untuk melanjutkan")
+# Load Hashed Passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
 
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    login_button = st.button("Login")
+credentials = {"usernames":{}}
 
-    if login_button:
-        user = login(username, password)
-        if user:
-            st.success("Login berhasil!")
-            # Redirect ke halaman selanjutnya setelah login berhasil
-            st.write("Halaman selanjutnya...")
+for un, name, pw in zip(usernames, names, hashed_passwords):
+    user_dict = {"name":name,"password":pw}
+    credentials["usernames"].update({un:user_dict})
+
+authenticator = stauth.Authenticate(credentials, "data_mancing", "abcdef", cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password salah")
+if authentication_status == None:
+    st.warning("Masukan Username dan Password")
+
+if authentication_status:
+
+    # Function to get latitude and longitude using Bing Maps API
+    def get_coordinates(location):
+        bing_maps_api_key = "Ag3lpNHBmRK6m0_yR0F-0IkUghCAglU5Yl4x8a0oJbkCoCB6-4Ha_UQelLt7C6SK"  # Replace with your Bing Maps API key
+        url = f"https://dev.virtualearth.net/REST/v1/Locations?q={location}&key={bing_maps_api_key}"
+        response = requests.get(url)
+        data = response.json()
+        
+        if response.status_code == 200 and data.get('resourceSets'):
+            coordinates = data['resourceSets'][0]['resources'][0]['point']['coordinates']
+            return coordinates  # Return latitude and longitude as [latitude, longitude]
         else:
-            st.error("Login gagal. Silakan coba lagi.")
+            return None
 
-# Fungsi untuk tampilan halaman utama
-def main_page():
-    st.title("Halaman Utama")
-    st.markdown("## Selamat datang di Journey Mancing!")
-    st.markdown("Silakan login untuk memulai.")
+    # Function to get weather info from a suitable weather API using latitude and longitude
+    def get_weather_info(latitude, longitude):
+        # Implement logic to fetch weather info from a weather API using latitude and longitude
+        # Use an appropriate weather API (e.g., OpenWeatherMap, WeatherAPI, etc.)
+        # Replace the code here with the API call to get weather information
 
-# Logika untuk menampilkan halaman login atau halaman utama berdasarkan status login
-is_logged_in = False  # Ganti dengan fungsi cek login yang sesuai
-if is_logged_in:
-    main_page()
-else:
-    login_page()
+        # Example fake weather data
+        fake_weather = {
+            "temperature": "25°C",
+            "condition": "Sunny",
+            "wind_speed": "5 m/s"
+        }
+
+        return fake_weather
+
+
+    # Fungsi untuk verifikasi login
+    def verify_login(username, password):
+        # Ganti URL dengan URL file txt di GitHub yang berisi username dan password
+        url = 'https://raw.githubusercontent.com/strng-fer/tubesalpro/main/user.txt'
+        response = requests.get(url)
+        credentials = response.text.split('\n')
+        
+        for cred in credentials:
+            if ',' in cred:
+                stored_username, stored_password = cred.split(',')
+                if username == stored_username and password == stored_password:
+                    return True
+        
+        return False
+
+    # Fungsi untuk menambahkan catatan memancing
+    def add_note():
+        st.title("Tambah Catatan Mancing")
+        
+        # Memasukkan foto
+        uploaded_file = st.file_uploader("Unggah Foto", type=['jpg', 'png'])
+
+        # Memasukkan detail lokasi
+        location_details = st.text_input("Detail Lokasi")
+
+        # Memasukkan lokasi pada map untuk mendapatkan latitude dan longitude
+        location = st.text_input("Cari Lokasi")
+        if location:
+            coordinates = get_coordinates(location)
+            if coordinates:
+                latitude, longitude = coordinates
+                st.write("Latitude:", latitude, "Longitude:", longitude)
+        # Memasukkan tanggal dan waktu
+        date_time = st.date_input("Tanggal") + st.time_input("Waktu")
+
+        # Memasukkan jenis ikan yang ditangkap
+        fish_type = st.text_input("Jenis Ikan yang Ditangkap")
+
+        # Memasukkan metode memancing
+        fishing_method = st.text_input("Metode Memancing")
+
+        # Tombol untuk menyimpan catatan memancing
+        if st.button("Simpan Catatan"):
+            # Simpan catatan memancing ke database atau file
+            st.success("Catatan Mancing Disimpan")
+
+            # Mendapatkan info cuaca
+            weather_info = get_weather_info(latitude, longitude)
+            st.subheader("Info Cuaca")
+            st.write(f"Temperatur: {weather_info['temperature']}")
+            st.write(f"Kondisi Cuaca: {weather_info['condition']}")
+            st.write(f"Kecepatan Angin: {weather_info['wind_speed']}")
+
+    # Fungsi untuk mengedit catatan
+    def edit_note():
+        st.title("Edit Catatan")
+        # Implementasikan logika untuk mengedit catatan di sini
+
+    # Fungsi untuk menghapus catatan
+    def delete_note():
+        st.title("Hapus Catatan")
+        # Implementasikan logika untuk menghapus catatan di sini
+
+    def login_page():
+        st.title("Login")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+
+        if st.button("Login"):
+            if verify_login(username, password):
+                st.success("Login berhasil!")
+                return True  # Kembalikan True jika login berhasil
+            else:
+                st.error("Username atau password salah")
+        
+        return False  # Kembalikan False jika login gagal
+
+    authenticator.logout("Logout","sidebar")
+    st.sidebar.title(f"Welcome {name}")
+
+    def main_page():
+        st.title("Halaman Utama")
+        option = st.selectbox("Pilih Operasi", ["Tambah Catatan", "Edit Catatan", "Hapus Catatan"])
+
+        if option == "Tambah Catatan":
+            add_note()
+        elif option == "Edit Catatan":
+            edit_note()
+        elif option == "Hapus Catatan":
+            delete_note()
+
+    
+    def main():
+        main_page()
+
+    if __name__ == "__main__":
+        main()
+    
