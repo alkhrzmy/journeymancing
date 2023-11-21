9775c8d2933c5ea245c2d5bd5b87e007aa70f3ca
Feryadi Yulius <133149348+strng-fer@users.noreply.github.com>
2023-11-21 23:17:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -110,7 +110,7 @@ if authentication_status:
         fishing_method = st.text_input("Metode Memancing")
 
         # Tombol untuk menyimpan catatan memancing
-        repo = git.Repo('https://raw.githubusercontent.com/alkhrzmy/journeymancing/edit/main/catatan_mancing.csv')  # Ganti dengan path ke repositori lokal Anda
+        repo = git.Repo('https://raw.githubusercontent.com/alkhrzmy/journeymancing/main/catatan_mancing.csv')  # Ganti dengan path ke repositori lokal Anda
 
         if st.button("Simpan Catatan"):
             note_data = [combined_datetime, fish_type, fishing_method, location_details]
