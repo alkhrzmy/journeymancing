1c1db471e2d87d780ee2dfacc4b6c7dc7a87d180
Feryadi Yulius <133149348+strng-fer@users.noreply.github.com>
2023-11-22 11:41:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,43 +99,22 @@ def add_note():
             map: map,
             anchorPoint: new google.maps.Point(0, -29)
         });
-    
-        autocomplete.addListener('place_changed', function() {
-            infowindow.close();
+
+        google.maps.event.addListener(map, 'click', function(event) {
             marker.setVisible(false);
-            var place = autocomplete.getPlace();
-    
-            if (place.geometry.viewport) {
-                map.fitBounds(place.geometry.viewport);
-            } else {
-                map.setCenter(place.geometry.location);
-                map.setZoom(17);
-            }
-            marker.setIcon({
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(35, 35)
-            });
-            marker.setPosition(place.geometry.location);
+            marker.setPosition(event.latLng);
             marker.setVisible(true);
     
-            var address = '';
-            if (place.address_components) {
-                address = [
-                    (place.address_components[0] && place.address_components[0].short_name || ''),
-                    (place.address_components[1] && place.address_components[1].short_name || ''),
-                    (place.address_components[2] && place.address_components[2].short_name || '')
-                ].join(' ');
-            }
+            var clickedLat = event.latLng.lat();
+            var clickedLng = event.latLng.lng();
     
-            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+            // Tampilkan info di info window
+            infowindow.setContent('<div>Clicked Location<br>Latitude: ' + clickedLat + '<br>Longitude: ' + clickedLng);
             infowindow.open(map, marker);
     
-            document.getElementById('location-snap').innerHTML = place.formatted_address;
-            document.getElementById('lat-span').innerHTML = place.geometry.location.lat();
-            document.getElementById('lon-span').innerHTML = place.geometry.location.lng();
+            // Simpan koordinat di elemen HTML
+            document.getElementById('lat-span').innerHTML = clickedLat;
+            document.getElementById('lon-span').innerHTML = clickedLng;
         });
     }
     </script>
