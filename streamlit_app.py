a85c0221611abb2ac19825d1bc0b3add2eadc1fb
Feryadi Yulius <133149348+strng-fer@users.noreply.github.com>
2023-11-25 06:42:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -202,7 +202,8 @@ def add_note(conn1, init_uploaded_file_="", init_location_details="", init_combi
         cursor = conn.cursor()
         cursor.execute(
             "INSERT INTO catatan(uploaded_file_name, uploaded_file_data, location_details, datetime, fish_type, bait_used, fishing_method) VALUES(?,?,?,?,?,?)",
-            (uploaded_file.name, sqlite3.Binary(uploaded_file.read(), uploaded_file, location_details_, datetime_, fish_type_, bait_used_, fishing_method_),)
+            (uploaded_file.name, sqlite3.Binary(uploaded_file.read()), uploaded_file, location_details_, datetime_, fish_type_, bait_used_, fishing_method_),
+            )
         st.success("Catatan baru tersimpan")
 
 # Fungsi untuk mengecek catatan
