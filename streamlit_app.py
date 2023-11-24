8cecc2a6a80af460de16624b863427c0a01771f8
Feryadi Yulius <133149348+strng-fer@users.noreply.github.com>
2023-11-25 01:24:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,11 +31,7 @@ def get_weather_info(latitude, longitude):
 def get_clicked_coordinates():
     return components.html('<script>getClickedCoordinates();</script>', height=0)
 
-df = pd.DataFrame([
-   {"foto": "url", "rating": 4, "is_widget": True},
-   {"foto": "url", "rating": 5, "is_widget": False},
-   {"foto": "url", "rating": 3, "is_widget": True},
-])
+df = pd.DataFrame([])
 
 # Fungsi untuk menambahkan catatan memancing
 def add_note():
